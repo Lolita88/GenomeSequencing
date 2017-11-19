5690e4f55343c8723c1d8b612d4137ee623e2594
Laura Lannon <lauralannon@Laura-Lannons-MacBook-Air-2.local>
2017-11-19 14:07:33 -0500

    Adds random to the start node

--- eulerian_cycle.py
+++ eulerian_cycle.py
@@ -5,27 +5,27 @@ import random
 
 def create_adjacency_my_list(my_list):
     #saving my_list into a dictionary
-    adj_my_list = {}
+    adj_list = {}
     circuit_max = 0
     for line in my_list:
         node = line.strip('\n')
         node = node.replace(' -> ', ' ')
         node = node.split(' ')
-        adj_my_list.setdefault(node[0],[]) #adj my_list gets the start of the node pair(ie. first num)
+        adj_list.setdefault(node[0],[]) #adj my_list gets the start of the node pair(ie. first num)
         for num in node[1].split(','): #num gets assigned the end node of the pair. Split on comma needed when multiple end nodes
-            adj_my_list[node[0]].append(num)
+            adj_list[node[0]].append(num)
             circuit_max += 1
-    #print(adj_my_list)
+    #print(adj_list)
     #print(circuit_max)
-    return adj_my_list, circuit_max
+    return adj_list, circuit_max
 
 def find_eulerian_cycle(my_list):
     #create adj my_list
-    adj_my_list, circuit_max = create_adjacency_my_list(my_list)
+    adj_list, circuit_max = create_adjacency_my_list(my_list)
 
     #reduced adj my_list to keep track of traveled edges
-    red_adj_my_list = {}
-    red_adj_my_list = deepcopy(adj_my_list) #exact copy of dict
+    red_adj_list = {}
+    red_adj_list = deepcopy(adj_list) #exact copy of dict
     #arbitrary starting point (if graph is directed/balanced)
     #hello
     start = random.choice(list(my_list))
@@ -35,12 +35,12 @@ def find_eulerian_cycle(my_list):
     stack = []
     circuit = []
     while len(circuit) != circuit_max:
-        if red_adj_my_list[curr_vert] != []: #if neighbor nodes exist
+        if red_adj_list[curr_vert] != []: #if neighbor nodes exist
             stack.append(curr_vert)
-            pick = randint(0,len(red_adj_my_list[curr_vert])-1) #what is pick?
+            pick = randint(0,len(red_adj_list[curr_vert])-1) #what is pick?
             temp = deepcopy(curr_vert) #why use deepcopy
-            curr_vert = red_adj_my_list[temp][pick]
-            red_adj_my_list[temp].remove(curr_vert)
+            curr_vert = red_adj_list[temp][pick]
+            red_adj_list[temp].remove(curr_vert)
         else:
             circuit.append(curr_vert)
             curr_vert = stack[len(stack)-1]
