98dbaee918298e93cf0733a4de30b32f220842d0
Laura Lannon <lauralannon@Laura-Lannons-MacBook-Air-2.local>
2018-01-09 10:56:38 -0500

    Adds still working on fixing errors for large data set in "visited" and "isolated_cycle" functions

--- maximal_non_branching_paths.py
+++ maximal_non_branching_paths.py
@@ -16,26 +16,19 @@ def maximal_non_branching_paths(adjacency_list):
     
     degree = in_and_out_degree(graph)
     paths = []
-
-    #print(degree)
-
     # for each node in gragh
     for v,e in graph.items():
-        #print(v)
         # if v is not a 1-in-1-out node
         if not one_in_one_out(v, degree):
-            print("hi")
-            print(str(v) + ' is NOT one in one out ') # 2/6/7 are one in one out, 1/3 not
             # if out(v) > 0
-            print(degree[1].get(v,0))
+            #print(degree[1].get(v,0))
             if degree[1].get(v,0) > 0:
-                
                 # for each outgoing edge (v, w) from v
                 for w in graph[v]: # for v = 3 it will be 2 paths
                     # NonBranchingPath ← the path consisting of the single edge (v, w)
-                    print("w " + str(w))
+                    #print("w " + str(w))
                     non_branching_path = str(v) + " -> " + str(w)
-                    #print(non_branching_path)
+                    #print("non_branching_path " + str(non_branching_path))
                     # while w is a 1-in-1-out node
                     while one_in_one_out(w, degree): #!!!! w is not a 1in out node!!!!
                         # extend NonBranchingPath by the outgoing edge (w, u) from w 
@@ -44,47 +37,65 @@ def maximal_non_branching_paths(adjacency_list):
                             #extend NonBranchingPath by the outgoing edge (w, u) from w 
                             non_branching_path += " -> " + u
                             w = u # breaks infinite loop but overwriting w
+                    #print("non_branching_path " + str(non_branching_path))
                     paths.append(non_branching_path)
-                    print("paths " + paths)
         else: # if isololated path (all nodes should be 1-in-1-out)
+            # at this point, still a lot of v's
             if not visited(v, paths): 
-                #print(v)
-                paths.append(isolated_cycle(v, degree, graph))
+                print("not visited v's " + str(v)) # this is where it's wrong
+                temp_cycle = isolated_cycle(v, degree, graph)
+                if temp_cycle:
+                    paths.append(temp_cycle)
     return paths
-    #print("paths " + str(paths))       
     #for each isolated cycle Cycle in Graph - isolated cycle - all nodes are 1-in-1-out nodes
         #add Cycle to Paths
     #return Paths
     
 def one_in_one_out(vertex, degree):
+    
     deg_in = degree[0].get(vertex,0)
     deg_out = degree[1].get(vertex,0)
-    print("deg_in " + str(deg_in))
-    print("deg_out " + str(deg_out))
+    #print("deg_in " + str(deg_in))
+    #print("deg_out " + str(deg_out))
     return (deg_in == 1) and (deg_out == 1)
 
 def visited(vertex, paths):
     # checks for vertex in paths
-    #print("paths " + str(paths))
+    print("paths " + str(paths))
+    # this works for large data set
     for path in paths:
-        #print(path)
-        #print(paths)
+        """if vertex == "344":
+            print("path " + path)
+            print("vertex " + vertex)"""
+        
         if vertex in path:
+            if vertex == "344":
+                print("in path " + path)
+                print("in vertex " + vertex)
+                """if vertex == "340":
+                print("in path " + path)
+                print("in vertex " + vertex)"""
             return True
+        #else:
+            #print("out path " + path)
+            #print("out vertex " + vertex)
     return False
 
 def isolated_cycle(vertex, degree, graph):
+    #print("vertex " + vertex)
     cycle = [vertex]
     while one_in_one_out(cycle[-1], degree):
         cycle.append(graph[cycle[-1]][0])
-        if cycle[0]==cycle[-1]:
+        #print("cycle " + str(cycle))
+        
+        if cycle[0]==cycle[-1]: # if first == last, end of
             # format this
             cycle_path = ""
             for each in cycle:
                 cycle_path += each + " -> "
             #print(cycle_path)
             return cycle_path[:-4]
-            #return cycle
+            #return cycle"""
     return None
     
 def in_and_out_degree(graph):
@@ -108,7 +119,6 @@ adjacency_list = [
 ]
 """
 adjacency_list = [line.strip() for line in open("files/maximal_non_branching_paths_data.txt")]
-#print(adjacency_list)
 
 print('\n'.join(maximal_non_branching_paths(adjacency_list)))
 #print(maximal_non_branching_paths(adjacency_list))
