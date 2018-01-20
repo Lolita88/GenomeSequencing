68994783718e56540d8ac22d9e762469df9d06ba
Laura Lannon <lauralannon@Laura-Lannons-MacBook-Air-2.local>
2018-01-20 18:02:26 -0500

    Adds cleanup, notes about general functionality, and stdin/out testing capability

--- eulerian_cycle.py
+++ eulerian_cycle.py
@@ -1,10 +1,14 @@
+"""
+Input: The adjacency list of an Eulerian directed graph.
+Output: An Eulerian cycle in this graph.
+"""
+
 from copy import deepcopy
 from random import randint
-import random
+import random, sys
 
 
 def create_adjacency_my_list(my_list):
-    #saving my_list into a dictionary
     adj_list = {}
     circuit_max = 0
     for line in my_list:
@@ -15,19 +19,15 @@ def create_adjacency_my_list(my_list):
         for num in node[1].split(','): #num gets assigned the end node of the pair. Split on comma needed when multiple end nodes
             adj_list[node[0]].append(num)
             circuit_max += 1
-    #print(adj_list)
-    #print(circuit_max)
     return adj_list, circuit_max
 
 def find_eulerian_cycle(my_list):
-    #create adj my_list
     adj_list, circuit_max = create_adjacency_my_list(my_list)
 
     #reduced adj my_list to keep track of traveled edges
     red_adj_list = {}
     red_adj_list = deepcopy(adj_list) #exact copy of dict
     #arbitrary starting point (if graph is directed/balanced)
-    #hello
     start = random.choice(list(my_list))
     start = start.split(" ")
     start = start[0]
@@ -37,8 +37,8 @@ def find_eulerian_cycle(my_list):
     while len(circuit) != circuit_max:
         if red_adj_list[curr_vert] != []: #if neighbor nodes exist
             stack.append(curr_vert)
-            pick = randint(0,len(red_adj_list[curr_vert])-1) #what is pick?
-            temp = deepcopy(curr_vert) #why use deepcopy
+            pick = randint(0,len(red_adj_list[curr_vert])-1) 
+            temp = deepcopy(curr_vert) 
             curr_vert = red_adj_list[temp][pick]
             red_adj_list[temp].remove(curr_vert)
         else:
@@ -51,7 +51,8 @@ def find_eulerian_cycle(my_list):
     for vert in circuit[::-1]:
         path += (vert + '->')
     return path.strip('->')
-
+"""
+#Sample Input:
 my_list = [
     "0 -> 3",
      "1 -> 0",
@@ -63,7 +64,17 @@ my_list = [
      "7 -> 9",
      "8 -> 7",
      "9 -> 6"]
-
+"""
 #my_list = open('files/eulerian_cycle.txt', 'r')
 
+data = []
+for line in sys.stdin:
+    data.append(line)
+my_list = data
+
 print(find_eulerian_cycle(my_list))
+
+"""
+Sample Output:
+2->1->0->3->2->6->8->7->9->6->5->4->2
+"""
